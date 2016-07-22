9a8de0a12beea346d09af1465453a7d8688c7b9b
Erikang20 <erikang20@gmail.com>
2016-07-22 18:19:42 -0500

    delete extra files

--- public/main.js
+++ public/main.js
@@ -1,13 +1,30 @@
+var app = angular.module( "myApp", [ 'ngRoute' ] );
+
+app.config( function ( $routeProvider ) {
+	$routeProvider.when( '/', {
+			templateUrl: 'partials/home.html',
+			controller: 'headerController'
+		} )
+		.when( '/about', {
+			templateUrl: 'partials/about.html',
+			controller: 'aboutController'
+		} )
+		.when( '/contact', {
+			templateUrl: '/partials/contact.html',
+			controller: 'contactController'
+		} )
+} );
+
 // parallax section
-$( document ).ready( function() {
+$( document ).ready( function () {
 	$( window ).trigger( 'scroll' );
-	$( '.parallax-window' ).load( function( parallax ) {
+	$( '.parallax-window' ).load( function ( parallax ) {
 		naturalWidth: 600,
 		naturalHeight: 400
 	} );
 } );
 
 
-$( document ).ready( function() {
+$( document ).ready( function () {
 	$( ".button-collapse" ).sideNav();
 } );
