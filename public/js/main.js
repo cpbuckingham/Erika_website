1df403bbc663f3c7c07d9f4d5ccec101dc404740
Erikang20 <erikang20@gmail.com>
2016-07-22 19:11:25 -0500

    add parallax

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,24 @@
-/* ========================================================================= */
-/*	Preloader
-/* ========================================================================= */
-
-jQuery( window ).load( function () {
-
-	$( "#preloader" ).fadeOut( "slow" );
-
-} );
-
-
-// parallax section
-$( document ).ready( function () {
-	$( window ).trigger( 'scroll' );
-	$( '.parallax-window' ).load( function ( parallax ) {
-		naturalWidth: 600,
-		naturalHeight: 400
-	} );
-} );
-
-
-$( document ).ready( function () {
-	$( ".button-collapse" ).sideNav();
-} );
+// /* ========================================================================= */
+// /*	Preloader
+// /* ========================================================================= */
+//
+// jQuery( window ).load( function () {
+//
+// 	$( "#preloader" ).fadeOut( "slow" );
+//
+// } );
+//
+//
+// // parallax section
+// $( document ).ready( function () {
+// 	$( window ).trigger( 'scroll' );
+// 	$( '.parallax-window' ).load( function ( parallax ) {
+// 		naturalWidth: 600,
+// 		naturalHeight: 400
+// 	} );
+// } );
+//
+//
+// $( document ).ready( function () {
+// 	$( ".button-collapse" ).sideNav();
+// } );
