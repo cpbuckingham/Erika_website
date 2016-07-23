fa358855262a7475dbf754230f0b1ee739702164
Erikang20 <erikang20@gmail.com>
2016-07-23 18:33:02 -0500

    angular no working

--- public/main.js
+++ public/main.js
@@ -1,27 +1,27 @@
 var app = angular.module( "myApp", [ 'ngRoute' ] );
 
 app.config( function ( $routeProvider ) {
-
     $routeProvider.when( '/', {
             templateUrl: 'partials/home.html',
-            controller: 'headerController'
+            controller: 'HeaderController'
         } )
         .when( '/contact', {
             templateUrl: '/partials/home.html',
-            controller: 'contactController'
-        } )
+            controller: 'ContactController'
+
+        } );
 } );
 
-app.controller( 'headerController', function ( $scope, $http ) {
+app.controller( 'HeaderController', function ( $scope, $http ) {
     $scope.view = {};
     $http.get( '/' ).then( function mySucces( response ) {
         console.log( "home page" );
-    } )
+    } );
 } );
 
-app.controller( 'contactController', function ( $scope, $http ) {
+app.controller( 'ContactController', function ( $scope, $http ) {
     $scope.view = {};
     $http.get( '/contact' ).then( function mySucces( response ) {
         console.log( "contact page" );
-    } )
+    } );
 } );
