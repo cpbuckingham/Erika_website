7005bfeb88281fc4823397a724f16320e7fd2250
Erikang20 <erikang20@gmail.com>
2016-07-23 18:03:41 -0500

    add style

--- public/main.js
+++ public/main.js
@@ -1,32 +1,28 @@
 var app = angular.module( "myApp", [ 'ngRoute' ] );
 
-
 app.config( function ( $routeProvider ) {
-  $routeProvider.when( '/', {
-			templateUrl: 'partials/home.html',
-			controller: 'headerController';
-} )
-.when( '/contact', {
-  templateUrl: '/partials/home.html',
-			controller: 'contactController';
-} )
+
+    $routeProvider.when( '/', {
+        templateUrl: 'partials/home.html',
+        controller: 'headerController'
+    } )
+
+    .when( '/contact', {
+        templateUrl: '/partials/home.html',
+        controller: 'contactController'
+    } );
 } );
 
 app.controller( 'headerController', function ( $scope, $http ) {
-  $scope.view = {
-};
-
-	$http.get( '/' )
-		.then( function mySucces( response ) {
-  console.log( "home page" );
-} );
+    $scope.view = {};
+    $http.get( '/' ).then( function mySucces( response ) {
+        console.log( "home page" );
+    } );
 } );
 
 app.controller( 'contactController', function ( $scope, $http ) {
-  $scope.view = {
-};
-	$http.get( '/contact' )
-		.then( function mySucces( response ) {
-  console.log( "contact page" );
-} )
-} )
+    $scope.view = {};
+    $http.get( '/contact' ).then( function mySucces( response ) {
+        console.log( "contact page" );
+    } );
+} );
