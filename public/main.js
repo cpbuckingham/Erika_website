242b55d63b54cec7e9a37eb4febe5ee8efedd1da
Erikang20 <erikang20@gmail.com>
2016-07-23 18:10:00 -0500

    add changes

--- public/main.js
+++ public/main.js
@@ -3,26 +3,25 @@ var app = angular.module( "myApp", [ 'ngRoute' ] );
 app.config( function ( $routeProvider ) {
 
     $routeProvider.when( '/', {
-        templateUrl: 'partials/home.html',
-        controller: 'headerController'
-    } )
-
-    .when( '/contact', {
-        templateUrl: '/partials/home.html',
-        controller: 'contactController'
-    } );
+            templateUrl: 'partials/home.html',
+            controller: 'headerController'
+        } )
+        .when( '/contact', {
+            templateUrl: '/partials/home.html',
+            controller: 'contactController'
+        } )
 } );
 
 app.controller( 'headerController', function ( $scope, $http ) {
     $scope.view = {};
     $http.get( '/' ).then( function mySucces( response ) {
         console.log( "home page" );
-    } );
+    } )
 } );
 
 app.controller( 'contactController', function ( $scope, $http ) {
     $scope.view = {};
     $http.get( '/contact' ).then( function mySucces( response ) {
         console.log( "contact page" );
-    } );
+    } )
 } );
