d10120cf761df66f5e7085aa8a8b9d1d01f60718
Erikang20 <erikang20@gmail.com>
2016-07-23 16:59:42 -0500

    add style

--- public/main.js
+++ public/main.js
@@ -2,27 +2,31 @@ var app = angular.module( "myApp", [ 'ngRoute' ] );
 
 
 app.config( function ( $routeProvider ) {
-	$routeProvider.when( '/', {
+  $routeProvider.when( '/', {
 			templateUrl: 'partials/home.html',
-			controller: 'headerController'
-		} )
-		.when( '/contact', {
-			templateUrl: '/partials/home.html',
-			controller: 'contactController'
-		} )
+			controller: 'headerController';
+} )
+.when( '/contact', {
+  templateUrl: '/partials/home.html',
+			controller: 'contactController';
+} )
 } );
 
 app.controller( 'headerController', function ( $scope, $http ) {
-	$scope.view = {};
+  $scope.view = {
+};
 
-	$http.get( '/' ).then( function mySucces( response ) {
-		console.log( "home page" );
-	} );
+	$http.get( '/' )
+		.then( function mySucces( response ) {
+  console.log( "home page" );
+} );
 } );
 
 app.controller( 'contactController', function ( $scope, $http ) {
-	$scope.view = {};
-	$http.get( '/contact' ).then( function mySucces( response ) {
-		console.log( "contact page" );
-	} );
-} );
+  $scope.view = {
+};
+	$http.get( '/contact' )
+		.then( function mySucces( response ) {
+  console.log( "contact page" );
+} )
+} )
--- routes/contact.js
+++ routes/contact.js
@@ -7,9 +7,7 @@ var bodyParser = require( 'body-parser' );
 
 
 router.get( '/', function ( req, res ) {
-	// knex( 'ErikaAngarita' ).select().then( function ( result, err ) {
 	res.render( 'index' );
-	// } );
 } );
 
 
