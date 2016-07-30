99627cc632e8f4fd65fe99dcf02de4f4a3da6c44
Erikang20 <erikang20@gmail.com>
2016-07-30 18:00:08 -0500

    change style

--- public/main.js
+++ public/main.js
@@ -1,16 +1,31 @@
-var app = angular.module( 'myApp', [ 'ngRoute' ] );
-
-app.config( function ( $routeProvider ) {
-    $routeProvider.when( '/', {
-        templateUrl: 'partials/home.html',
-        controller: 'HeaderController'
-
-    } );
+$( window ).load( function () {
+    //
+    $( "#preloader" ).fadeOut( "slow" );
 } );
 
-app.controller( 'HeaderController', function ( $scope, $http ) {
-    // $scope.view = {};
-    $http.get( '/' ).then( function mySucces( response ) {
-        console.log( "home page" );
+$( document ).ready( function () {
+    console.log( "here" );
+    $( ".button-collapse" ).sideNav();
+} );
+
+
+// $( document ).ready( function ( parallax ) {
+$( window ).trigger( 'scroll' );
+$( '#scene' ).load( function ( parallax ) {
+    //         // naturalWidth: 600,
+    //         // naturalHeight: 400
+    $( '#scene' ).parallax( {
+        calibrateX: false,
+        calibrateY: true,
+        invertX: false,
+        invertY: true,
+        limitX: false,
+        limitY: 10,
+        scalarX: 2,
+        scalarY: 8,
+        frictionX: 0.2,
+        frictionY: 0.8,
+        originX: 0.0,
+        originY: 1.0
     } );
 } );
