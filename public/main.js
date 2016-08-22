90ee856ba066d3ac799269d526a79efb550958da
Erikang20 <erikang20@gmail.com>
2016-08-22 18:57:47 -0500

    fixed the thing

--- public/main.js
+++ public/main.js
@@ -6,16 +6,19 @@ $( document ).ready( function () {
 
     $( '.modal-trigger' ).leanModal();
 
+
     $( '.open-overlay' ).click( function () {
+
         $( '.open-overlay' ).css( 'pointer-events', 'none' );
         var overlay_navigation = $( '.overlay-navigation' ),
+
             top_bar = $( '.bar-top' ),
             middle_bar = $( '.bar-middle' ),
             bottom_bar = $( '.bar-bottom' );
 
         overlay_navigation.toggleClass( 'overlay-active' );
-        if ( overlay_navigation.hasClass( 'overlay-active' ) ) {
 
+        if ( overlay_navigation.hasClass( 'overlay-active' ) ) {
             top_bar.removeClass( 'animate-out-top-bar' ).addClass( 'animate-top-bar' );
             middle_bar.removeClass( 'animate-out-middle-bar' ).addClass( 'animate-middle-bar' );
             bottom_bar.removeClass( 'animate-out-bottom-bar' ).addClass( 'animate-bottom-bar' );
@@ -36,12 +39,12 @@ $( document ).ready( function () {
                             } );
                             $( '.open-overlay' ).css( 'pointer-events', 'auto' );
                         }
-                    } )
+                    } );
                 }
-            } )
+            } );
 
         } else {
-            $( '.open-overlay' ).css( 'pointer-events', 'none' );
+            $( '.open-overlay' ).css( 'pointer-events', 'auto' );
             top_bar.removeClass( 'animate-top-bar' ).addClass( 'animate-out-top-bar' );
             middle_bar.removeClass( 'animate-middle-bar' ).addClass( 'animate-out-middle-bar' );
             bottom_bar.removeClass( 'animate-bottom-bar' ).addClass( 'animate-out-bottom-bar' );
@@ -67,6 +70,20 @@ $( document ).ready( function () {
             } );
         }
     } );
+
+    $( 'nav ul li a' ).click( function () {
+        var overlay_navigation = $( '.overlay-navigation' );
+        $( this ).toggleClass( 'clicked' );
+
+        // overlay_navigation.removeClass( 'overlay-slide-down' ).addClass( 'overlay-slide-up' )
+        overlay_navigation.velocity( 'transition.fadeOut', {
+            delay: 0,
+            duration: 300,
+        } );
+        $( 'overlay_navigation' ).hide();
+        console.log( "delaying" );
+    } );
+
 
 
     console.log( "here" );
